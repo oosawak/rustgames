cda10a12f841e2eade8cbee7ce6837ccc39d44d0
oosawak <oosawakou@gmail.com>
2026-07-19 14:37:29 +0900

    Move MP bar next to HP bar horizontally

--- wasm_roguelike/src/state.rs
+++ wasm_roguelike/src/state.rs
@@ -923,20 +923,20 @@ pub fn render_canvas(game: &RoguelikeGame, canvas_id: &str, width: i32, height: 
                     28.0,
                 ).ok();
 
-                // Draw MP bar below HP
+                // Draw MP bar next to HP
                 ctx.set_fill_style(&"#333".into());
-                ctx.fill_rect(5.0, 35.0, 150.0, 30.0);
+                ctx.fill_rect(160.0, 5.0, 150.0, 30.0);
 
                 ctx.set_fill_style(&"#00f".into());
                 let mp_width = (game.mp as f64 / game.max_mp as f64) * 140.0;
-                ctx.fill_rect(10.0, 40.0, mp_width, 10.0);
+                ctx.fill_rect(165.0, 10.0, mp_width, 10.0);
 
                 ctx.set_fill_style(&"#fff".into());
                 ctx.set_font("12px monospace");
                 ctx.fill_text(
                     &format!("MP: {}/{}", game.mp, game.max_mp),
-                    15.0,
-                    58.0,
+                    170.0,
+                    28.0,
                 ).ok();
 
                 // Draw enemy HP and name
